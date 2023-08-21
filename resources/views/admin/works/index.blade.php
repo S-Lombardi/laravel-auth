701c48d69a4b34a5dc554276e1772b77f2e35b0b
S-Lombardi <silo.lombardi@gmail.com>
2023-08-21 16:58:16 +0200

    Aggiunti file di autenticazione

--- resources/views/admin/works/index.blade.php
+++ resources/views/admin/works/index.blade.php
@@ -1,8 +1,16 @@
-@extendes('layouts.admin')
+@extends('layouts.admin')
 
 @section('content')
     <div>
-        
+        <div class="container">
+            <div class="row">
+                @foreach($works as $project)
+                    <div class="col-12 card">
+                        {{$project->title}}
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\Admin\DashboardController as DashboardCrontroller;
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
 use App\Http\Controllers\Admin\PortfolioController as PortfolioController;
 
 /*
@@ -21,17 +20,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-//Dopo aver creato il controlle, gestisco la rrotta con il controller stesso
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function(){
     Route::get('/',[DashboardController::class,'index'])->name('dashboard');
-    Route::resource('works', PortfolioController::class);
+    Route::resource('works',PortfolioController::class);
 
 });
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

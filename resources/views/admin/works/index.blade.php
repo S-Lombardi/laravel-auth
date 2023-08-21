aadb3c6cec6f07e0bbd455695dbf384e7f9d396b
S-Lombardi <silo.lombardi@gmail.com>
2023-08-21 17:00:47 +0200

    Cilco for in index

--- resources/views/admin/works/index.blade.php
+++ resources/views/admin/works/index.blade.php
@@ -4,9 +4,12 @@
     <div>
         <div class="container">
             <div class="row">
+                {{-- Mostro a video i dati creati con Faker --}}
                 @foreach($works as $project)
                     <div class="col-12 card">
                         {{$project->title}}
+
+                        
                     </div>
                 @endforeach
             </div>

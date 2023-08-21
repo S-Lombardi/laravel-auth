ef0928cefc0aee3a1219da499d538c903cdc1b45
S-Lombardi <silo.lombardi@gmail.com>
2023-08-21 19:27:50 +0200

    Aggiunti i dati del progetto nel file show

--- resources/views/admin/works/index.blade.php
+++ resources/views/admin/works/index.blade.php
@@ -33,6 +33,7 @@
                                     Dettagli-->
                                 </a>
 
+
                             </div>
                         </div>
 
--- resources/views/admin/works/show.blade.php
+++ resources/views/admin/works/show.blade.php
@@ -2,8 +2,42 @@
 
 @section('content')
     <div class="container">
+        <div class="row mt-5">
+            {{-- COL LEFT - IMMAGINE--}}
+            <div class="col-6">
+                
+                {{-- IMMAGINE --}}
+                <div class="card-img-top">
+                    <img class="img-fluid" src="{{$work->image}}" alt="{{$work->title}}">
+                </div>
 
-        {{$work->title}}
+            </div>
+
+            {{-- COL RIGHT - DESCRIZIONE--}}
+            <div class="col-6">
+                {{-- TITOLO --}}
+                <h5 class="card-title text-uppercase mb-2">{{$work->title}}</h5>
+
+                {{-- DESCRIZIONE --}}
+                <p class="card-text">{{$work->description}}</p>
+
+                {{-- LINK --}}
+                <div class="mb-3">   
+                    Link: 
+                    <em><a href="#">{{$work->link}}</a></em>
+                </div>
+
+                {{--FIGURE PROFESSIONALI del progetto--}}
+
+                <div>Back-ender: {{$work->back_ender}}</div>
+
+                <div>Front-ender: {{$work->front_ender}}</div>
+
+            </div>
+
+        </div>
+
+
     </div>
 
 @endsection
